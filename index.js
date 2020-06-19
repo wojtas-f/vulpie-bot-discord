8da44992b74985c00cc742444aabd8d4abefbbc9
Vulpie <wojtas_f@outlook.com>
2020-06-19 22:02:47 +0200

    feat: add basic bot response

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const client = new discord.Client()
 
 
 
-client.once('ready', () => console.log('Vulpie bot is ALIVE!!!'))
+client.on('ready', () => console.log('Vulpie bot is ALIVE!!!'))
 
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!')
+    }
+})
 
-
-
+client.on('message', msg => {
+    if (msg.content === 'what is my avatar') {
+        msg.reply(msg.author.displayAvatarURL())
+    }
+})
 
 
 client.login(process.env.BOT_TOKEN) // must be the last line 
